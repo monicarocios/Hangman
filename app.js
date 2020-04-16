3fa5010a1cc9afb647c2f7b68f9b2d85833e5d75
Monica Scroggs <scrmr0210@gmail.com>
2020-04-16 01:29:23 -0400

    added alphabet buttons to gaming section

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const game = () => {
 	// Start the Game
 	const startGame = () => {
 		const playbtn = document.getElementById('intro-btn');
-		const introScreen = document.querySelector('.jumbotron');
+		const introScreen = document.querySelector('.intro');
 		const gameScreen = document.querySelector('.game');
 
 		playbtn.addEventListener('click', () => {
@@ -13,7 +13,57 @@ const game = () => {
 		});
 	};
 
+	const printButtons = () => {
+		let alphabet = [
+			'a',
+			'b',
+			'c',
+			'd',
+			'e',
+			'f',
+			'g',
+			'h',
+			'i',
+			'j',
+			'k',
+			'l',
+			'm',
+			'n',
+			'o',
+			'p',
+			'q',
+			'r',
+			's',
+			't',
+			'u',
+			'v',
+			'w',
+			'x',
+			'y',
+			'z'
+		];
+
+		for (i = 0; i < alphabet.length; i++) {
+			// let name = document.createTextNode(alphabet[i]);
+			let btn = document.createElement('button');
+			btn.type = 'button';
+			btn.className = 'btn btn-light';
+			let name = document.createTextNode(alphabet[i]);
+			btn.appendChild(name);
+			document.getElementById('alphabet').appendChild(btn);
+		}
+	};
+
 	startGame();
+	printButtons();
+
+	// draw different parts of hangman when guess incorrectly
+	// functions divided by parts
+
+	const drawHangman = () => {
+		var canvas = document.getElementById('myCanvas');
+		var ctx = canvas.getContext('2d');
+	};
 };
 
 game();
